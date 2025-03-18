d5ecef30c83d8c1d8b1d6ef912782acf8a3fbb44
Flyoftime <techtasker.mirsabanwar@gmail.com>
2025-03-18 22:09:08 +0700

    feat/IP-6-create-crud-role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JenisController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,6 +24,16 @@ Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::controller(JenisController::class)->prefix('jenis')->name('jenis.')->group(function () {
         Route::get('/', 'index')->name('index');
         Route::get('/create', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
+});
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    Route::controller(RoleController::class)->prefix('role')->name('role.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::get('/edit', 'edit')->name('edit');
         Route::post('/', 'store')->name('store');
         Route::put('/', 'update')->name('update');
         Route::delete('/', 'destroy')->name('destroy');
