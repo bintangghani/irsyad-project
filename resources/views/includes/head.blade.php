e2e1dc831bf5dea5738e1b3e2ccf918712b4450c
Nabil <muhammadnabilathillah@gmail.com>
2025-05-10 23:49:57 +0700

    t

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="{{ asset('storage/' . ($setting->icon ? $setting->icon : 'default-icon.png')) }}" type="image/x-icon"/>
 <title>{{ $setting->title ?? 'PUSKITA' }}</title>
 
-<link rel="icon" href="{{ asset('/storage/' . ($setting->icon ?? 'default-icon.png')) }}" type="image/x-icon"/>
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
